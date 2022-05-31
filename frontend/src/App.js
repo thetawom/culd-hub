26b211a352f87734b01b9bced9158e7e3b5f9835
Ethan Wu <ethan.yj.wu@gmail.com>
2022-05-31 15:06:51 +0800

    Set up Apollo middleware for authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_USERS = gql`
+	{
+		users {
+			id
+			firstName
+			lastName
+		}
+	}
+`;
 
 function App() {
-	return <div className="App"></div>;
+	const { loading, error, data } = useQuery(GET_USERS);
+	if (loading) return "Loading ...";
+	if (error) return `Error! ${error.message}`;
+	console.log(data);
+	return (
+		<div className="App">
+			{data.users.map(({ id, firstName, lastName }) => (
+				<p key={id}>
+					{firstName} {lastName}
+				</p>
+			))}
+		</div>
+	);
 }
 
 export default App;
