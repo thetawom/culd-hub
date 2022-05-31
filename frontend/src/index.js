32931a812227852a3f1186121d50621da68c22f0
Ethan Wu <ethan.yj.wu@gmail.com>
2022-05-31 15:06:51 +0800

    Set up Apollo middleware for authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { setContext } from "@apollo/client/link/context";
+import { AUTH_TOKEN } from "./constants";
+import {
+	ApolloClient,
+	InMemoryCache,
+	createHttpLink,
+	ApolloProvider,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+	uri: "http://localhost:8000/graphql/",
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `JWT ${token}` : "",
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>
 );
