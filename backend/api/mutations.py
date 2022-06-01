21be5e5ce22df85eb03338b85c560cbb2eaac4f6
Ethan Wu <ethan.yj.wu@gmail.com>
2022-06-02 03:46:46 +0800

    Add show sign-up and de-registering functions

--- backend/api/mutations.py
+++ backend/api/mutations.py
@@ -1,6 +1,7 @@
 import graphene
 from users.models import User
-from .types import UserType
+from show_manager.models import Show, Member, Role
+from .types import RoleType, UserType
 
 
 class CreateUserMutation(graphene.Mutation):
@@ -24,3 +25,35 @@ class CreateUserMutation(graphene.Mutation):
         user_instance.set_password(password)
         user_instance.save()
         return CreateUserMutation(user=user_instance)
+
+
+class CreateRoleMutation(graphene.Mutation):
+    role = graphene.Field(RoleType)
+
+    class Arguments:
+        show_id = graphene.ID(required=True)
+
+    @staticmethod
+    def mutate(root, info, show_id):
+        role_instance = Role(
+            show=Show.objects.get(pk=show_id),
+            performer=Member.objects.get(pk=info.context.user.member.id),
+        )
+        role_instance.save()
+        return CreateRoleMutation(role=role_instance)
+
+
+class DeleteRoleMutation(graphene.Mutation):
+    role = graphene.Field(RoleType)
+
+    class Arguments:
+        show_id = graphene.ID(required=True)
+
+    @staticmethod
+    def mutate(root, info, show_id):
+        role_instance = Role.objects.get(
+            show=Show.objects.get(pk=show_id),
+            performer=Member.objects.get(pk=info.context.user.member.id),
+        )
+        role_instance.delete()
+        return DeleteRoleMutation(role=role_instance)
