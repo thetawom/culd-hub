31c1d314570426b4a81bfddef40b88fcddfb932b
Ethan Wu <ethan.yj.wu@gmail.com>
2022-06-03 12:02:21 +0800

    Refactor show table components

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,20 +1,13 @@
 import { gql } from "@apollo/client";
 import React, { useContext, useState } from "react";
-import {
-	Divider,
-	Layout,
-	Typography,
-	Segmented,
-	Button,
-	Space,
-	Tooltip,
-} from "antd";
-import { SyncOutlined } from "@ant-design/icons";
+import { Divider, Layout, Typography } from "antd";
 import AuthContext from "../context/AuthContext";
 import useAuthQuery from "../utils/useAuthQuery";
 import Header from "../components/Header";
 import ShowsTable from "../components/ShowsTable";
 import Loader from "../components/Loader";
+import { ShowsTableProvider } from "../context/ShowsTableContext";
+import ShowsTableControls from "../components/ShowsTableControls";
 
 const GET_ME_QUERY = gql`
 	{
@@ -38,10 +31,6 @@ const HomePage = () => {
 		onError: () => logoutUser(),
 	});
 
-	let [openFilter, setOpenFilter] = useState("Open");
-
-	let [refreshed, setRefreshed] = useState(false);
-
 	return (
 		<Layout style={{ minHeight: "100vh" }}>
 			<Header />
@@ -55,32 +44,16 @@ const HomePage = () => {
 				{loading ? (
 					<Loader />
 				) : (
-					<>
+					<ShowsTableProvider>
 						<div style={{ display: "flex", justifyContent: "space-between" }}>
 							<Typography.Title level={2} style={{ marginBottom: "0em" }}>
 								{`Welcome, ${user.firstName}!`}
 							</Typography.Title>
-							<Space style={{ marginTop: "auto" }}>
-								<Tooltip title="Refetch shows" placement="bottom">
-									<Button onClick={() => setRefreshed(false)}>
-										<SyncOutlined />
-									</Button>
-								</Tooltip>
-								<Segmented
-									options={["Open", "Closed", "All"]}
-									value={openFilter}
-									onChange={setOpenFilter}
-								/>
-							</Space>
+							<ShowsTableControls />
 						</div>
 						<Divider style={{ marginTop: "0.8em", marginBottom: "1.2em" }} />
-						<ShowsTable
-							user={user}
-							openFilter={openFilter}
-							refreshed={refreshed}
-							setRefreshed={setRefreshed}
-						/>
-					</>
+						<ShowsTable user={user} />
+					</ShowsTableProvider>
 				)}
 			</Layout.Content>
 		</Layout>
