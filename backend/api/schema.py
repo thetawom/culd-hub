ad1179c285b5149c3a3120fba9170e2b08369117
Ethan Wu <ew2664@columbia.edu>
2022-07-31 13:13:28 -0700

    Add query for constant choices in show manager models

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,10 +1,13 @@
+import json
+
 import graphene
 import graphql_jwt
 from django.dispatch import receiver
 from graphql_jwt.decorators import login_required, staff_member_required
 from graphql_jwt.refresh_token.signals import refresh_token_rotated
 
-from show_manager.models import Member, Show
+from show_manager.models import Member, Show, SCHOOL_CHOICES, CLASS_YEAR_CHOICES, MEMBERSHIP_CHOICES, \
+    SHOW_PRIORITY_CHOICES, PERFORMANCE_ROLE_CHOICES
 from users.models import User
 from .mutations import CreateUserMutation, CreateRoleMutation, DeleteRoleMutation, EditUserMutation
 from .types import UserType, MemberType, ShowType
@@ -15,27 +18,61 @@ def revoke_refresh_token(sender, request, refresh_token, **kwargs):
     refresh_token.revoke(request)
 
 
+def tuple_to_json(tuple_dict):
+    return json.dumps(dict((k, v) for k, v in tuple_dict))
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
     members = graphene.List(MemberType)
     shows = graphene.List(ShowType)
     me = graphene.Field(UserType)
 
+    school_choices = graphene.String()
+    class_year_choices = graphene.String()
+    membership_choices = graphene.String()
+    show_priority_choices = graphene.String()
+    performance_role_choices = graphene.String()
+
+    @staticmethod
     @staff_member_required
-    def resolve_users(self, info, **kwargs):
+    def resolve_users(root, info, **kwargs):
         return User.objects.all()
 
+    @staticmethod
     @login_required
-    def resolve_members(self, info, **kwargs):
+    def resolve_members(root, info, **kwargs):
         return Member.objects.all()
 
+    @staticmethod
     @login_required
-    def resolve_shows(self, info, **kwargs):
+    def resolve_shows(root, info, **kwargs):
         return Show.objects.filter(is_published=True)
 
+    @staticmethod
     @login_required
-    def resolve_me(self, info, **kwargs):
+    def resolve_me(root, info, **kwargs):
         return User.objects.get(pk=info.context.user.pk)
+
+    @staticmethod
+    def resolve_school_choices(root, info, **kwargs):
+        return tuple_to_json(SCHOOL_CHOICES)
+
+    @staticmethod
+    def resolve_class_year_choices(root, info, **kwargs):
+        return tuple_to_json(CLASS_YEAR_CHOICES)
+
+    @staticmethod
+    def resolve_membership_choices(root, info, **kwargs):
+        return tuple_to_json(MEMBERSHIP_CHOICES)
+
+    @staticmethod
+    def resolve_show_priority_choices(root, info, **kwargs):
+        return tuple_to_json(SHOW_PRIORITY_CHOICES)
+
+    @staticmethod
+    def resolve_performance_role_choices(root, info, **kwargs):
+        return tuple_to_json(PERFORMANCE_ROLE_CHOICES)
 
 
 class Mutation(graphene.ObjectType):
