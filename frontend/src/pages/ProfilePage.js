e357d675905342876fc1ec1a0290674c5bf7e908
Ethan Wu <ew2664@columbia.edu>
2022-08-06 17:45:29 -0700

    Add Form for each profile item

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from "react";
-import {Divider, Input, Layout, Select, Space, Typography} from "antd";
+import {Divider, Form, Input, Layout, Select, Space, Typography} from "antd";
 import {UserOutlined} from "@ant-design/icons";
 import UserContext from "../context/UserContext";
 import Header from "../components/Header";
@@ -67,16 +67,17 @@ const ProfilePage = () => {
                         value={`${user.firstName} ${user.lastName}`}
                         input={
                             <Input.Group compact style={{width: "calc(100% - 50px)"}}>
-                                <Input
-                                    placeholder="First name"
-                                    defaultValue={user.firstName}
-                                    style={{width: "50%"}}
-                                />
-                                <Input
-                                    placeholder="Last name"
-                                    defaultValue={user.lastName}
-                                    style={{width: "50%"}}
-                                />
+                                <Form.Item name="firstName" initialValue={user.firstName}>
+                                    <Input
+                                        placeholder="First name"
+                                        style={{width: "50%"}}
+                                    /></Form.Item>
+                                <Form.Item name="lastName" initialValue={user.lastName}>
+                                    <Input
+                                        placeholder="Last name"
+                                        style={{width: "50%"}}
+                                    />
+                                </Form.Item>
                             </Input.Group>
                         }
                     />
@@ -84,22 +85,24 @@ const ProfilePage = () => {
                         title="Email Address"
                         value={user.email}
                         input={
-                            <Input
-                                placeholder="Email address"
-                                defaultValue={user.email}
-                                style={{width: "calc(100% - 50px)"}}
-                            />
+                            <Form.Item name="email" initialValue={user.email}>
+                                <Input
+                                    placeholder="Email address"
+                                    style={{width: "calc(100% - 50px)"}}
+                                />
+                            </Form.Item>
                         }
                     />
                     <ProfileItem
                         title="Phone Number"
                         value={user.phone || "Not set"}
                         input={
-                            <Input
-                                placeholder="Phone number"
-                                defaultValue={user.phone}
-                                style={{width: "calc(100% - 50px)"}}
-                            />
+                            <Form.Item name="phone" initialValue={user.phone}>
+                                <Input
+                                    placeholder="Phone number"
+                                    style={{width: "calc(100% - 50px)"}}
+                                />
+                            </Form.Item>
                         }
                     />
                     <ProfileItem
@@ -107,19 +110,18 @@ const ProfilePage = () => {
                         value={user.member.school || "Not set"}
                         choices={schoolChoices}
                         input={
-                            <Select
-                                placeholder="School"
-                                defaultValue={schoolChoices ?
-                                    schoolChoices[user.member.school] ?? user.member.school
-                                    : user.member.school}
-                                style={{width: "calc(100% - 50px)"}}
-                            >
-                                <>
-                                    {Object.entries(schoolChoices).map(([key, value]) =>
-                                        <Select.Option key={key} value={key}>{value.toString()}</Select.Option>
-                                    )}
-                                </>
-                            </Select>
+                            <Form.Item name="school" initialValue={user.member.school}>
+                                <Select
+                                    placeholder="School"
+                                    style={{width: "calc(100% - 50px)"}}
+                                >
+                                    <>
+                                        {Object.entries(schoolChoices).map(([key, value]) =>
+                                            <Select.Option key={key} value={key}>{value.toString()}</Select.Option>
+                                        )}
+                                    </>
+                                </Select>
+                            </Form.Item>
                         }
                     />
                     <ProfileItem
@@ -127,19 +129,18 @@ const ProfilePage = () => {
                         value={user.member.classYear || "Not set"}
                         choices={classYearChoices}
                         input={
-                            <Select
-                                placeholder="Class year"
-                                defaultValue={classYearChoices ?
-                                    classYearChoices[user.member.classYear] ?? user.member.school
-                                    : user.member.classYear}
-                                style={{width: "calc(100% - 50px)"}}
-                            >
-                                <>
-                                    {Object.entries(classYearChoices).map(([key, value]) =>
-                                        <Select.Option key={key} value={key}>{value.toString()}</Select.Option>
-                                    )}
-                                </>
-                            </Select>
+                            <Form.Item name="classYear" initialValue={user.member.classYear}>
+                                <Select
+                                    placeholder="Class year"
+                                    style={{width: "calc(100% - 50px)"}}
+                                >
+                                    <>
+                                        {Object.entries(classYearChoices).map(([key, value]) =>
+                                            <Select.Option key={key} value={key}>{value.toString()}</Select.Option>
+                                        )}
+                                    </>
+                                </Select>
+                            </Form.Item>
                         }
                     />
                 </Space>
@@ -149,133 +150,3 @@ const ProfilePage = () => {
 };
 
 export default ProfilePage;
-
-// eslint-disable-next-line
-{
-    /* <Form form={form} name="register" onFinish={onFinish}>
-<Form.Item>
-    <Input.Group compact>
-        <Form.Item
-            name="first_name"
-            rules={[
-                {
-                    required: true,
-                    message: "Please enter your first name.",
-                },
-            ]}
-            normalize={toTitleCase}
-            noStyle
-        >
-            <Input
-                prefix={<UserOutlined />}
-                placeholder="First name"
-                style={{ width: "50%" }}
-            />
-        </Form.Item>
-        <Form.Item
-            name="last_name"
-            rules={[
-                { required: true, message: "Please enter your last name." },
-            ]}
-            normalize={toTitleCase}
-            noStyle
-        >
-            <Input placeholder="Last name" style={{ width: "50%" }} />
-        </Form.Item>
-    </Input.Group>
-</Form.Item>
-<Form.Item
-    name="email"
-    rules={[
-        {
-            type: "email",
-            message: "This is not a valid email address.",
-        },
-        { required: true, message: "Please enter your email address." },
-    ]}
-    validateStatus={invalidEmail ? "error" : ""}
-    hasFeedback={invalidEmail}
-    onChange={onChange}
-    normalize={toLowerCase}
->
-    <Input prefix={<MailOutlined />} placeholder="Email address" />
-</Form.Item>
-<Form.Item
-    name="phone_number"
-    rules={[
-        {
-            pattern: new RegExp(
-                /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/g
-            ),
-            message: "This is not a valid phone number.",
-        },
-    ]}
->
-    <Input placeholder="Phone number" prefix={<PhoneOutlined />} />
-</Form.Item>
-<Form.Item
-    name="password"
-    rules={[
-        { required: true, message: "Please enter your password." },
-    ]}
-    hasFeedback
->
-    <Input.Password
-        prefix={<LockOutlined />}
-        placeholder="Password"
-    />
-</Form.Item>
-<Form.Item
-    name="confirm"
-    depndencies={["password"]}
-    rules={[
-        { required: true, message: "Please confirm your password." },
-        ({ getFieldValue }) => ({
-            validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                }
-
-                return Promise.reject(
-                    new Error(
-                        "The two passwords that you entered do not match."
-                    )
-                );
-            },
-        }),
-    ]}
-    hasFeedback
->
-    <Input.Password
-        prefix={<LockOutlined />}
-        placeholder="Confirm password"
-    />
-</Form.Item>
-<Form.Item shouldUpdate>
-    {() => (
-        <Button
-            type="primary"
-            htmlType="submit"
-            disabled={
-                !form.isFieldsTouched(
-                    [
-                        "first_name",
-                        "last_name",
-                        "email",
-                        "password",
-                        "confirm",
-                    ],
-                    true
-                ) ||
-                !!form
-                    .getFieldsError()
-                    .filter(({ errors }) => errors.length).length
-            }
-            style={{ width: "100%" }}
-        >
-            Register
-        </Button>
-    )}
-</Form.Item>
-</Form> */
-}
