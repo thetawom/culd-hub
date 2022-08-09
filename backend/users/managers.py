68f31c051e93f73330197b8895e848fb75f21420
Ethan Wu <ew2664@columbia.edu>
2022-08-09 09:08:21 -0700

    Test commit

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -19,8 +19,8 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+        # if extra_fields.get("is_staff") is not True:
+        #     raise ValueError(_("Superuser must have is_staff=True."))
+        # if extra_fields.get("is_superuser") is not True:
+        #     raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create(email, password, **extra_fields)
