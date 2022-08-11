eeb9e7b86a066cb643d1e20a33828071dbf51d3c
Ethan Wu <ew2664@columbia.edu>
2022-08-11 11:32:01 -0700

    Update GitHub django workflow

---
+++ backend/core/settings/default.py
@@ -0,0 +1,8 @@
+from core.settings.base import *
+
+SECRET_KEY = "SECRET_KEY"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["backend", "localhost", "127.0.0.1"]
