dc4b14205dadfed4e71b54220e70396372965caf
Ethan Wu <64718929+ew2664@users.noreply.github.com>
2022-08-15 14:47:43 -0700

    Fix slack.py codestyle

--- backend/slack.py
+++ backend/slack.py
@@ -4,14 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class SlackBoss(object):
     def __new__(cls):
-        if not hasattr(cls, 'instance'):
+        if not hasattr(cls, "instance"):
             cls.instance = super(SlackBoss, cls).__new__(cls)
             cls.instance.client = WebClient(token=os.getenv("SLACK_TOKEN"))
         return cls.instance
+
     def create_channel(self, name):
-        return self.client.conversations_create(
-            name=name,
-            is_private=False
-        )
+        return self.client.conversations_create(name=name, is_private=False)
