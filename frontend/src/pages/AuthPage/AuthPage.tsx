eab34b6e71076687badaecaaf00897033a5cb02c
Ethan Wu <ethan.yj.wu@gmail.com>
2022-08-30 12:41:34 -0700

    Use CSS module for AuthPage

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Divider, Image, Typography} from "antd";
 import logo from "../../assets/logo.png";
+import styles from "./AuthPage.module.css";
 
 interface Props {
     title?: string,
@@ -11,55 +12,19 @@ interface Props {
 
 const AuthPage: React.FC<Props> = ({title, subtitle, alert, children}) => {
     return (
-        <div
-            style={{
-                width: "100vw",
-                height: "100vh",
-                backgroundColor: "#F8F8F8",
-            }}
-        >
-            <div
-                style={{
-                    width: "80%",
-                    maxWidth: "400px",
-                    minWidth: "300px",
-                    backgroundColor: "white",
-                    padding: "30px",
-                    paddingBottom: "15px",
-                    borderRadius: "5px",
-                    position: "absolute",
-                    left: "50%",
-                    top: "50%",
-                    transform: "translate(-50%, -55%)",
-                }}
-            >
-                <div
-                    style={{
-                        textAlign: "center",
-                        marginTop: "10px",
-                        marginBottom: "20px",
-                    }}
-                >
-                    <Image
-                        width={150}
-                        src={logo}
-                        style={{
-                            borderRadius: "20px",
-                        }}
-                        preview={false}
-                    />
+        <div className={styles.background}>
+            <div className={styles.panel}>
+                <div className={styles.logo}>
+                    <Image width={150} src={logo} preview={false}/>
                 </div>
-                <Typography.Title
-                    level={1}
-                    style={{textAlign: "center", marginBottom: "0.2em"}}
-                >
+                <Typography.Title level={1} className={styles.title}>
                     {title ? title : "CU Lion Dance"}
                 </Typography.Title>
-                <Typography style={{textAlign: "center", marginBottom: "1em"}}>
+                <Typography className={styles.subtitle}>
                     {subtitle}
                 </Typography>
                 {alert}
-                <Divider style={{marginTop: "6px"}}/>
+                <Divider className={styles.divider}/>
                 {children}
             </div>
         </div>
