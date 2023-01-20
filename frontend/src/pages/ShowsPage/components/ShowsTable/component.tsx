9c2c70290189dade703399b4e38948409383b877
Ethan Wu <ew2664@columbia.edu>
2023-01-20 18:13:30 -0500

    Remove contact column from shows table

--- frontend/src/pages/ShowsPage/components/ShowsTable/component.tsx
+++ frontend/src/pages/ShowsPage/components/ShowsTable/component.tsx
@@ -1,28 +1,17 @@
 import React, {useContext} from "react";
-import {
-    Button,
-    message,
-    Modal,
-    Progress,
-    Space,
-    Table,
-    Tag,
-    Tooltip
-} from "antd";
+import {Button, Modal, Progress, Space, Table, Tag, Tooltip} from "antd";
 import {
     CarFilled,
     CrownFilled,
     InfoCircleOutlined,
     InfoCircleTwoTone,
-    MailTwoTone,
-    PhoneTwoTone,
     PlusOutlined,
     StarFilled,
     WarningFilled
 } from "@ant-design/icons";
 import dayjs, {Dayjs} from "dayjs";
 import ShowsTableContext from "../../context/ShowsTableContext";
-import {Contact, Member, Round, Show, User} from "../../../../types/types";
+import {Member, Round, Show, User} from "../../../../types/types";
 import {ShowContextInterface} from "../../context/ShowsTableContext/types";
 import ShowDetails from "../ShowDetails";
 import {Views} from "../ShowsTableControls";
@@ -169,114 +158,78 @@ const ShowsTable = ({user}: { user: User }) => {
             </div>)),
         sorter: (a, b) => a.time?.localeCompare(b.time),
     }, {
-        title: (<span
-            style={{
-                textAlign: "center", width: "100%", display: "inline-block",
-            }}
-        >
-					Lions
-				</span>),
+        title: (<span style={{
+            textAlign: "center",
+            width: "100%",
+            display: "inline-block"
+        }}>Lions</span>),
         dataIndex: "lions",
         key: "lions",
         width: "5%",
-        render: (lions: number) => (<span
-            style={{
-                textAlign: "center", width: "100%", display: "inline-block",
-            }}
-        >
-					{lions}
-				</span>),
-    },
-
-        {
-            title: "Contact",
-            dataIndex: "contact",
-            key: "contact",
-            render: (contact: Contact) => contact && (<>
-                <Tooltip
-                    title={contact.phone ? contact.phone.replace(/(\+1)(\d{3})(\d{3})(\d{4})/, "($2) $3-$4") : contact.email ? contact.email : null}
-                    placement="bottom"
-                    style={{textAlign: "center"}}
-                >
-                    <Tag
-                        icon={contact.phone ? (
-                            <PhoneTwoTone/>) : contact.email ? (
-                            <MailTwoTone/>) : null}
-                        style={{cursor: "pointer"}}
-                        color="blue"
-                        onClick={async () => {
-                            if (contact.phone) {
-                                await navigator.clipboard.writeText(contact.phone);
-                                await message.info("Phone number copied to clipboard");
-                            } else if (contact.email) {
-                                await navigator.clipboard.writeText(contact.email);
-                                await message.info("Email address copied to clipboard");
-                            }
-                        }}
-                    >
-                        {contact.firstName} {contact.lastName}
-                    </Tag>
+        render: (lions: number) => (<span style={{
+            textAlign: "center",
+            width: "100%",
+            display: "inline-block"
+        }}>{lions}</span>),
+    }, {
+        title: (<span>
+                {"Tentative Roster"}
+            <Tooltip title="Final confirmations via Slack"
+                     placement="right">
+                    <InfoCircleTwoTone
+                        style={{marginLeft: "6px", fontSize: "0.85em"}}
+                    />
                 </Tooltip>
-            </>),
-        }, {
-            title: (<span>
-					{"Tentative Roster"}
-                <Tooltip title="Final confirmations via Slack"
-                         placement="right">
-						<InfoCircleTwoTone
-                            style={{marginLeft: "6px", fontSize: "0.85em"}}
-                        />
-					</Tooltip>
-				</span>),
-            dataIndex: "performers",
-            key: "performers",
-            render: (performers: Member[], show: Show) => (
-                <div style={{display: "flex", justifyContent: "space-between"}}>
-                    <Space wrap>
-                        {performers
-                            .slice()
-                            .sort((a: Member, b: Member) => {
-                                return a.user.id === show.point?.user.id ? -1 : b.user.id === show.point?.user.id ? 1 : a.user.firstName.localeCompare(b.user.firstName);
-                            })
-                            .map((performer: Member) => (
-                                <Tooltip
-                                    title={`${performer.user.firstName} ${performer.user.lastName}${show.point?.user.id == performer.user.id ? " (point)" : ""}`}
-                                    placement="bottom"
-                                    key={performer.user.id}
+            </span>),
+        dataIndex: "performers",
+        key: "performers",
+        render: (performers: Member[], show: Show) => (
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Space wrap>
+                    {performers
+                        .slice()
+                        .sort((a: Member, b: Member) => {
+                            return a.user.id === show.point?.user.id ? -1 : b.user.id === show.point?.user.id ? 1 : a.user.firstName.localeCompare(b.user.firstName);
+                        })
+                        .map((performer: Member) => (
+                            <Tooltip
+                                title={`${performer.user.firstName} ${performer.user.lastName}${show.point?.user.id == performer.user.id ? " (point)" : ""}`}
+                                placement="bottom"
+                                key={performer.user.id}
+                            >
+                                <Tag
+                                    style={{
+                                        marginRight: "0px",
+                                        cursor: "default"
+                                    }}
+                                    color={performer.user.id === show.point?.user.id ? "volcano" : null}
                                 >
-                                    <Tag
-                                        style={{
-                                            marginRight: "0px",
-                                            cursor: "default"
-                                        }}
-                                        color={performer.user.id === show.point?.user.id ? "volcano" : null}
-                                    >
-                                        {performer.user.firstName}
-                                    </Tag>
-                                </Tooltip>))}
-                    </Space>
-                    <Tooltip
-                        title={show.lions ? `${performers.length} of ${performersNeeded(show)} performers` : `${performers.length} performer${performers.length == 1 ? "" : "s"}`}
-                        placement="bottom"
-                    >
-                        <Progress
-                            type="circle"
-                            percent={show.lions == null ? 0 : Math.round((performers.length / performersNeeded(show)) * 100)}
-                            format={() => `${performers.length}`}
-                            width={32}
-                            style={{
-                                marginLeft: "auto",
-                                marginRight: "10px",
-                                marginTop: "auto",
-                                marginBottom: "auto",
-                                paddingLeft: "10px",
-                                cursor: "default",
-                            }}
-                        />
-                    </Tooltip>
-                </div>),
-            width: "35%",
-        },];
+                                    {performer.user.firstName}
+                                </Tag>
+                            </Tooltip>))}
+                </Space>
+                <Tooltip
+                    title={show.lions ? `${performers.length} of ${performersNeeded(show)} performers` : `${performers.length} performer${performers.length == 1 ? "" : "s"}`}
+                    placement="bottom"
+                >
+                    <Progress
+                        type="circle"
+                        percent={show.lions == null ? 0 : Math.round((performers.length / performersNeeded(show)) * 100)}
+                        format={() => `${performers.length}`}
+                        width={32}
+                        style={{
+                            marginLeft: "auto",
+                            marginRight: "10px",
+                            marginTop: "auto",
+                            marginBottom: "auto",
+                            paddingLeft: "10px",
+                            cursor: "default",
+                        }}
+                    />
+                </Tooltip>
+            </div>),
+        width: "35%",
+    }];
 
     return view == Views.TABLE && <Table
         columns={columns}
@@ -289,3 +242,35 @@ const ShowsTable = ({user}: { user: User }) => {
 };
 
 export default ShowsTable;
+
+// {
+//     title: "Contact",
+//     dataIndex: "contact",
+//     key: "contact",
+//     render: (contact: Contact) => contact && (<>
+//         <Tooltip
+//             title={contact.phone ? contact.phone.replace(/(\+1)(\d{3})(\d{3})(\d{4})/, "($2) $3-$4") : contact.email ? contact.email : null}
+//             placement="bottom"
+//             style={{textAlign: "center"}}
+//         >
+//             <Tag
+//                 icon={contact.phone ? (
+//                     <PhoneTwoTone/>) : contact.email ? (
+//                     <MailTwoTone/>) : null}
+//                 style={{cursor: "pointer"}}
+//                 color="blue"
+//                 onClick={async () => {
+//                     if (contact.phone) {
+//                         await navigator.clipboard.writeText(contact.phone);
+//                         await message.info("Phone number copied to clipboard");
+//                     } else if (contact.email) {
+//                         await navigator.clipboard.writeText(contact.email);
+//                         await message.info("Email address copied to clipboard");
+//                     }
+//                 }}
+//             >
+//                 {contact.firstName} {contact.lastName}
+//             </Tag>
+//         </Tooltip>
+//     </>),
+// }
